506d876e27bf0f34ab9388e815fb1f2af94ef891
sarazafe <sara@blunder.es>
2020-11-11 21:21:32 +0100

    feat: Add constants needed to deal with the shelves and filter the books from the API by shelves

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+/**
+ * The available values for shelves
+ * @type {{read: string, currentlyReading: string, wantToRead: string}}
+ */
+export const Shelves = {
+	currentlyReading: 'currentlyReading',
+	wantToRead: 'wantToRead',
+	read: 'read',
+};
+
+/**
+ * The title per each shelf
+ * @type {{read: string, currentlyReading: string, wantToRead: string}}
+ */
+export const ShelvesTitle = {
+	currentlyReading: 'Currently Reading',
+	wantToRead: 'Want to Read',
+	read: 'Read',
+}
