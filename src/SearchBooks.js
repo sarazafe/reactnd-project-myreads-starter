3d388d5c11385eaf61027dc418b141602efb8a1a
sarazafe <sara@blunder.es>
2020-11-13 20:10:45 +0100

    feat: Search books and display the results

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,11 +1,33 @@
-import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component, Fragment} from 'react';
+import {Link} from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+import Book from "./Book";
 
 /**
  * Component that allows the user to search books and add it the his/her reads
  */
-class SearchBooks extends Component{
+class SearchBooks extends Component {
+	state = {
+		books: [],
+	};
+
+	/**
+	 * Handler for searching books when something is written on search input
+	 * @param event - the event with the new query
+	 */
+	handleChange = event => {
+		BooksAPI.search(event.target.value)
+			.then(books => {
+				this.setState(() => ({
+					books,
+				}));
+			});
+	};
+
 	render() {
+		const {books} = this.state;
+		console.log(books.length);
+		console.log(books);
 		return (<div className="search-books">
 			<div className="search-books-bar">
 				<Link
@@ -13,13 +35,28 @@ class SearchBooks extends Component{
 					className='close-search'
 				>Close</Link>
 				<div className="search-books-input-wrapper">
-					<input type="text" placeholder="Search by title or author"/>
+					<input
+						type="text"
+						placeholder="Name"
+						name="query"
+						onChange={this.handleChange}
+						placeholder="Search by title or author"/>
 				</div>
 			</div>
 			<div className="search-books-results">
-				<ol className="books-grid"></ol>
+				{
+					(books && books.length > 0) && (
+						<ol className="books-grid">
+							{books.map(book => (
+								<li key={book.id}><Book book={book} onSelectedShelf={() => {
+								}}/></li>
+							))}
+						</ol>
+					)
+				}
 			</div>
 		</div>);
 	}
 }
+
 export default SearchBooks;
