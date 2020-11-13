c1728a677b11df87b3492efc309969b973817932
sarazafe <sara@blunder.es>
2020-11-13 20:10:15 +0100

    feat: Customize some styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import * as BooksAPI from './BooksAPI'
-import './App.css'
+import React from 'react';
+import * as BooksAPI from './BooksAPI';
+import './App.css';
 import {MyReads} from "./MyReads";
 import SearchBooks from "./SearchBooks";
 import {Route} from 'react-router-dom';
