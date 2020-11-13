996e0ec1a7ecb6b6653dde22fff7def455d65319
sarazafe <sara@blunder.es>
2020-11-13 20:09:50 +0100

    feat: Add default value when no authors or no images

--- src/Book.js
+++ src/Book.js
@@ -16,15 +16,18 @@ class Book extends Component {
 		return (
 			<div className="book">
 				<div className="book-top">
-					<div className="book-cover" style={{
-						width: 128,
-						height: 193,
-						backgroundImage: `url(${book.imageLinks.thumbnail})`
-					}}></div>
+					{
+						book.imageLinks ? (
+							<div className="book-cover" style={{
+								backgroundImage: `url(${book.imageLinks.thumbnail})`
+							}}></div>
+						) : <div className="book-cover book-cover-title">{book.title}</div>
+					}
+
 					<BookShelfSelector book={book} onSelectedShelf={onSelectedShelf}/>
 				</div>
 				<div className="book-title">{book.title}</div>
-				<div className="book-authors">{book.authors.join(', ')}</div>
+				<div className="book-authors">{book.authors ? book.authors.join(', ') : '-'}</div>
 			</div>
 		);
 	}
